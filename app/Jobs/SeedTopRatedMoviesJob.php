c98496d875a5afb76dfe257f179632a6f8f46820
Mohammed Naguib <naguib@mojazinc.com>
2021-09-17 22:22:20 +0200

    updated-.env.example

--- app/Jobs/SeedTopRatedMoviesJob.php
+++ app/Jobs/SeedTopRatedMoviesJob.php
@@ -35,7 +35,7 @@ class SeedTopRatedMoviesJob implements ShouldQueue
         Movie::query()->delete();
         // first seed the no. of pages
         $count = 0;
-        $client = new \GuzzleHttp\Client(['base_uri' => "https://api.themoviedb.org/"]); //HTTP Client was introduced in laravel 7 so we have to use GuzzleHttp
+        $client = new \GuzzleHttp\Client(['base_uri' => config("moviedb.baseUrl")]); //HTTP Client was introduced in laravel 7 so we have to use GuzzleHttp
         for ($i=1;  $count < $this->numberOfMovies ; $i++) { //
             $response = $client->request("GET", "/3/movie/top_rated?api_key=".config("moviedb.key").'&page='.$i);
             $movies =(json_decode($response->getBody()->getContents()));
